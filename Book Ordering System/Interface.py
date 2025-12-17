0cdf88163312a8d9369268637d7e8935bc268df7
Alxks4o <yonashkovaleks07@gmail.com>
2025-12-17 14:43:42 +0000

    1.0

---
+++ Book Ordering System/Interface.py
@@ -0,0 +1,232 @@
+import tkinter as tk
+from tkinter import messagebox
+from add_customer import Customer
+
+def clearEntries(*entries):
+    for entry in entries:
+        entry.delete(0, tk.END)
+
+def addCustomerFunc():
+    firstName = first_name_entry.get().strip()
+    lastName = last_name_entry.get().strip()
+    email = email_entry.get().strip()
+
+    if not firstName or not lastName or not email:
+        print("Empty")
+        message_label.config(text="All fields are required.", fg="red")
+    else:
+        print("Something here.")
+        user = Customer(firstName, lastName, email)
+        user.createUser()
+
+        message_label.config(text="Customer added successfully!", fg="green")
+
+        clearEntries(first_name_entry, last_name_entry, email_entry)
+    
+
+def showFrame(frame):
+    frame.tkraise()
+
+#creating window
+window = tk.Tk()
+
+#setting window size
+window.geometry("800x500")
+window.minsize(800, 600)
+
+window.rowconfigure(0, weight=1)
+window.columnconfigure(1, weight=1)
+
+
+'''
+NAVIGATION
+'''
+
+navigation = tk.Frame(window, bg='#ffe9d6')
+navigation.grid(row=0, column=0, sticky='ns')
+
+
+
+#buttons quick design 
+button_style = {
+    "font": ("Arial", 14),
+    "height": 2,
+    "width": 14,
+    "bg": "#ffd3ad"
+}
+
+#HOME PAGE
+homeButton = tk.Button(
+    navigation,
+    **button_style,
+    text= "Home",
+    command=lambda: showFrame(home)
+).pack(fill='x')
+
+
+#button for adding customers 
+addCustomerButton = tk.Button(
+    navigation,
+    **button_style,
+    text= "Add Customer",
+    command=lambda: showFrame(addCustomer)
+).pack(fill='x')
+
+
+#button for adding books
+addBookButton = tk.Button(
+    navigation,
+    **button_style,
+    text= "Add Book",
+    command=lambda: showFrame(addBook)
+).pack(fill='x')
+
+
+#button for placing orders 
+placeOrderButton = tk.Button(
+    navigation,
+    **button_style,
+    text= "Place Order",
+    command=lambda: showFrame(placeOrder)
+).pack(fill='x')
+
+
+#button for viewing invoices 
+viewInvoicesButton = tk.Button(
+    navigation,
+    **button_style,
+    text= "View Invoices",
+    command=lambda: showFrame(viewInvoices)
+).pack(fill='x')
+
+
+#Button for searching through invoices 
+searchInvoicesButton = tk.Button(
+    navigation,
+    **button_style,
+    text= "Search Invoices",
+    command=lambda: showFrame(searchInvoices)
+).pack(fill='x')
+
+
+
+'''
+FRAMES
+'''
+
+#Dashboard buttons frame
+main = tk.Frame(window, bg="#f5e6c8")
+main.grid(row=0, column=1, sticky='nsew')
+
+
+home = tk.Frame(main, bg="#f5e6c8")
+home.grid(row=0, column=0, sticky='nsew')
+
+tk.Label(home, text="Welcome to Book Management", font="Arial, 20", bg="#f5e6c8").pack(padx=100, pady=40, anchor='center', expand=True)
+
+addCustomer = tk.Frame(main, bg="#f5e6c8")
+addCustomer.grid(row=0, column=0, sticky="nsew")
+
+# Title
+tk.Label(
+    addCustomer,
+    text="Add Customer",
+    font=("Arial", 25, "bold"),
+    bg="#f5e6c8"
+).grid(row=0, column=0, columnspan=2, pady=(40, 20))
+
+# First Name
+tk.Label(
+    addCustomer,
+    text="First Name",
+    font=("Arial", 15),
+    bg="#f5e6c8"
+).grid(row=1, column=0, sticky="w", padx=50, pady=10)
+
+first_name_entry = tk.Entry(addCustomer, font=("Arial", 15))
+first_name_entry.grid(row=2, column=0, padx=50, pady=10)
+
+# Last Name
+tk.Label(
+    addCustomer,
+    text="Last Name",
+    font=("Arial", 15),
+    bg="#f5e6c8"
+).grid(row=3, column=0, sticky="w", padx=50, pady=10)
+
+last_name_entry = tk.Entry(addCustomer, font=("Arial", 15))
+last_name_entry.grid(row=4, column=0, padx=50, pady=10)
+
+# Email
+tk.Label(
+    addCustomer,
+    text="Email",
+    font=("Arial", 15),
+    bg="#f5e6c8"
+).grid(row=5, column=0, sticky="w", padx=50, pady=10)
+
+email_entry = tk.Entry(addCustomer, font=("Arial", 15))
+email_entry.grid(row=6, column=0, padx=50, pady=10)
+
+# Error / Success message
+message_label = tk.Label(
+    addCustomer,
+    text="",
+    font=("Arial", 12),
+    fg="green",
+    bg="#f5e6c8"
+)
+message_label.grid(row=7, column=0, columnspan=2, pady=10)
+
+
+# Submit button
+submit_btn = tk.Button(
+    addCustomer,
+    text="Add Customer",
+    font=("Arial", 15),
+    command=addCustomerFunc
+)
+submit_btn.grid(row=8, column=0,sticky="nws", padx=50, pady=20)
+
+# Clear button
+clear_btn = tk.Button(
+    addCustomer,
+    text="Clear",
+    font=("Arial", 15),
+    command=lambda: clearEntries(first_name_entry, last_name_entry, email_entry)
+)
+clear_btn.grid(row=8, column=0, sticky="nes", padx=50, pady=20)
+
+
+addBook = tk.Frame(main, bg="#f5e6c8")
+addBook.grid(row=0, column=0, sticky='nsew')
+
+tk.Label(addBook, text="Add Book", font="Arial, 20", bg="#f5e6c8").pack(padx=100, pady=40, anchor='center', expand=True)
+
+
+
+placeOrder = tk.Frame(main, bg="#f5e6c8")
+placeOrder.grid(row=0, column=0, sticky='nsew')
+
+tk.Label(placeOrder, text="Place Order", font="Arial, 20", bg="#f5e6c8").pack(padx=100, pady=40, anchor='center', expand=True)
+
+
+
+
+viewInvoices = tk.Frame(main, bg="#f5e6c8")
+viewInvoices.grid(row=0, column=0, sticky='nsew')
+
+tk.Label(viewInvoices, text="View Invoices", font="Arial, 20", bg="#f5e6c8").pack(padx=100, pady=40, anchor='center', expand=True)
+
+
+
+searchInvoices = tk.Frame(main, bg="#f5e6c8")
+searchInvoices.grid(row=0, column=0, sticky='nsew')
+
+tk.Label(searchInvoices, text="Search Invoices", font="Arial, 20", bg="#f5e6c8").pack(padx=100, pady=40, anchor='center', expand=True)
+
+
+
+showFrame(addCustomer)
+
+window.mainloop()
